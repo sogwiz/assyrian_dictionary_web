33871a248a6ae73dd1f4701635a472ec1409b3e3
Sargon Benjamin <sargonb@amazon.com>
2025-11-03 21:05:23 -0800

    menu changes to add the Names feature as a top level feature

--- app/js/components/MenuBurger.js
+++ app/js/components/MenuBurger.js
@@ -11,10 +11,10 @@ class MenuBurger extends React.Component {
             <div id="outer-container" style={{height: '100%'}}>
         <Menu>
         <a id="home" className="menu-item" href="/">Search 🔎</a>
-        <a id="about" className="menu-item" href="/about">About 📖</a>
-        <a id="trends" className="menu-item" href="/trends">Trends 📊</a>
+        <a id="about" className="menu-item" href="/names">Names 👶</a>
         <a id="phrases" className="menu-item" href="/phrases">Phrases 🗣</a>
         <a id="proverbs" className="menu-item" href="/proverbs">Proverbs 📜</a>
+        <a id="about" className="menu-item" href="/about">About 📖</a>
         <a id="support" className="menu-item" href="/support">Support ♥</a>
       </Menu>
       <main id="page-wrap">
